2509b69fed196560b8cc2f5edc0322b99f84c3bd
Erik Karlsson <Erikdkarlsson8@gmail.com>
2016-03-31 18:13:49 -0700

    Wrote my first xcode / ios unit tests. Was pretty cool being able to execute just a specific test from the code editor by clicking on the diamond on the left hand side. About to starting the portion on creating a table view.

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -16,8 +16,16 @@ class Meal {
     var rating: Int
     
     // MARK: Initialization
-    init(name: String, photo: UIImage?, rating: Int){
-        
-    }
     
+    // Have to add the ? to mark the init keyword as failable
+    init?(name: String, photo: UIImage?, rating: Int){
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
 }
